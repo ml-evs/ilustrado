4e1fe8a439662f8c372225dcd144911b6434fcc0
Matthew Evans <me388@cam.ac.uk>
2017-04-22 18:38:01 +0100

    Plot 2D hull based on input hull

--- ilustrado/analysis.py
+++ ilustrado/analysis.py
@@ -26,3 +26,27 @@ def fitness_swarm_plot(generations):
     ax.set_xlabel('Generation number')
     ax.set_ylabel('Distance to initial hull (eV/atom)')
     plt.savefig(generations[0].run_hash + '_swarmplot.pdf')
+
+
+def plot_new_2d_hull(generations, hull):
+    """ Add new structures to old ConvexHull plot. """
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    hull.set_plot_param()
+    ax = hull.plot_2d_hull(show=False, ax=ax)
+    mutant_colours = plt.cm.Reds_r(np.linspace(0, 0.5, len(generations)))
+    crossover_colours = plt.cm.Blues_r(np.linspace(0, 0.5, len(generations)))
+    for idx, generation in enumerate(generations[1:]):
+        for structure in generation.populace:
+            if 'mutations' not in structure:
+                colour = mutant_colours[idx]
+            else:
+                colour = mutant_colours[idx] if 'crossover' not in structure['mutations'] else crossover_colours[idx]
+            ax.scatter(structure['concentration'], structure['formation_enthalpy_per_atom'],
+                       c=colour, s=35, zorder=1000, lw=0)
+    plt.savefig(generations[0].run_hash + '_hullplot.pdf')
