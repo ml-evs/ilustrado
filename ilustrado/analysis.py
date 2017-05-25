8434a3a532895acd8f56071e913b61d8ade51e07
Matthew Evans <me388@cam.ac.uk>
2017-05-25 12:16:37 +0100

    Changes to swarm plot

--- ilustrado/analysis.py
+++ ilustrado/analysis.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 """ Some assorted analysis functions. """
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def display_gen(generation):
@@ -7,16 +9,15 @@ def display_gen(generation):
     print(generation)
 
 
-def fitness_swarm_plot(generations):
+def fitness_swarm_plot(generations, ax=None, save=False):
     """ Make a swarm plot of the fitness of all generations. """
     import matplotlib
     matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    import seaborn as sns
     import numpy as np
     sns.set_palette("Dark2", desat=.5)
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111)
+    if ax is None:
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111)
     fitnesses = np.asarray([generation.raw_fitnesses for generation in generations
                             if len(generation) > 1]).T
     sns.violinplot(data=fitnesses, ax=ax, inner=None, color=".6")
@@ -25,15 +26,15 @@ def fitness_swarm_plot(generations):
                   palette=sns.color_palette("Dark2", desat=.5))
     ax.set_xlabel('Generation number')
     ax.set_ylabel('Distance to initial hull (eV/atom)')
-    plt.savefig(generations[0].run_hash + '_swarmplot.pdf')
+    if save:
+        plt.savefig(generations[0].run_hash + '_swarmplot.pdf')
+    return ax
 
 
 def plot_new_2d_hull(generations, hull):
     """ Add new structures to old ConvexHull plot. """
     import matplotlib
     matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    import seaborn as sns
     import numpy as np
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
@@ -47,6 +48,6 @@ def plot_new_2d_hull(generations, hull):
                 colour = mutant_colours[idx]
             else:
                 colour = mutant_colours[idx] if 'crossover' not in structure['mutations'] else crossover_colours[idx]
-            ax.scatter(structure['concentration'], structure['formation_enthalpy_per_atom'],
+            ax.scatter(structure['concentration'][0], structure['formation_enthalpy_per_atom'],
                        c=colour, s=35, zorder=1000, lw=0)
     plt.savefig(generations[0].run_hash + '_hullplot.pdf')
