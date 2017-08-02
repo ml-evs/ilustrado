d420c041b3b89637c29303175c119264a4272a2f
Matthew Evans <me388@cam.ac.uk>
2017-08-02 18:03:47 +0100

    Improved tests

--- ilustrado/tests/init_test.py
+++ ilustrado/tests/init_test.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 import unittest
 from os.path import realpath
-from os import chdir
-from os import uname
+from os import uname, remove, chdir
 from multiprocessing import cpu_count
+import glob
 REAL_PATH = '/'.join(realpath(__file__).split('/')[:-1]) + '/'
 
 
 def hull_test():
-    """ Perform a test run of Ilustrado with a low quality parameter set,
+    """ Perform a test run of Ilustrado with dummy DFT,
     on all cores of current machine.
     """
     from matador.query import DBQuery
     from matador.hull import QueryConvexHull
-    from matador.similarity.similarity import get_uniq_cursor
     from ilustrado.ilustrado import ArtificialSelector
 
     chdir(REAL_PATH)
@@ -28,7 +27,7 @@ def hull_test():
                     kpoint_tolerance=0.0, subcmd='hull', biggest=True, source=True)
     hull = QueryConvexHull(query,
                            subcmd='hull', no_plot=True, kpoint_tolerance=0.03, source=True,
-                           summary=False, hull_cutoff=5e-2)
+                           summary=True, hull_cutoff=0)
 
     # uniq_list, _, _, _ = list(get_uniq_cursor(hull.hull_cursor[1:-1], debug=False))
     cursor = hull.hull_cursor[1:-1]
@@ -47,9 +46,37 @@ def hull_test():
                        testing=True,
                        max_num_mutations=1,
                        mutation_rate=0.5, crossover_rate=0.5,
-                       num_generations=3, population=50,
-                       num_survivors=10, elitism=0.3,
+                       num_generations=3, population=15,
+                       num_survivors=10, elitism=0.5,
                        loglevel='debug')
+
+    run_hash = glob.glob('*.json')[0].split('-')[0]
+
+    new_life = ArtificialSelector(gene_pool=cursor,
+                                  seed='KP',
+                                  hull=hull,
+                                  debug=False,
+                                  fitness_metric='hull',
+                                  recover_from=run_hash,
+                                  load_only=True,
+                                  nprocs=cpus,
+                                  ncores=1,
+                                  testing=True,
+                                  max_num_mutations=1,
+                                  mutation_rate=0.5, crossover_rate=0.5,
+                                  num_generations=5, population=15,
+                                  num_survivors=10, elitism=0.5,
+                                  loglevel='debug')
+    print(len(new_life.generations))
+    print([len(new_life.generations[i]) for i in range(len(new_life.generations))])
+    print([len(new_life.generations[i].bourgeoisie) for i in range(len(new_life.generations))])
+    assert len(new_life.generations[-1]) >= 15
+    assert len(new_life.generations[-1].bourgeoisie) >= 10
+
+    new_life.start()
+
+    [remove(f) for f in glob.glob('*.json')]
+    [remove(f) for f in glob.glob('*.log')]
 
 
 class MatadorHullUnitTest(unittest.TestCase):
