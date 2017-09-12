dfe915c7d18b0953f59d66f4b870166c3eda7c83
Matthew Evans <me388@cam.ac.uk>
2017-09-12 16:25:26 +0100

    New plotting style for finalised hulls

--- ilustrado/analysis.py
+++ ilustrado/analysis.py
@@ -26,13 +26,14 @@ def fitness_swarm_plot(generations, ax=None, save=False):
     return ax
 
 
-def plot_new_2d_hull(generations, hull):
+def plot_new_2d_hull(generations, hull, points=True, label_hull=True, save=True):
     """ Add new structures to old ConvexHull plot. """
     import numpy as np
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
     hull.set_plot_param()
-    ax = hull.plot_2d_hull(show=False, ax=ax, plot_points=False)
+    ax = hull.plot_2d_hull(show=False, ax=ax, plot_points=points, labels=label_hull)
+    ax2 = ax.twinx()
     mutant_colours = plt.cm.PRGn(np.linspace(0, 0.3, len(generations)))
     crossover_colours = plt.cm.PRGn_r(np.linspace(0, 0.3, len(generations)))
     for structure in generations[0].populace:
@@ -46,12 +47,18 @@ def plot_new_2d_hull(generations, hull):
                 colour = mutant_colours[idx] if 'crossover' not in structure['mutations'] else crossover_colours[idx]
             ax.scatter(structure['concentration'][0], structure['formation_enthalpy_per_atom'],
                        c=colour, s=35, zorder=1000, lw=0)
-    colours = ['red', mutant_colours[0], mutant_colours[-1], crossover_colours[0], crossover_colours[-1]]
-    labels = ['initial populace', 'mutant gen 1', 'mutant final gen', 'crossover gen 1', 'crossover final gen']
+    colours = ['red', mutant_colours[0], crossover_colours[0]]
+    labels = ['initial populace', 'mutant', 'crossover']
     for ind, colour in enumerate(colours):
         if ind == 0:
-            ax.scatter(-0.5, 10, c=colour, s=50, zorder=1000, lw=2, edgecolors='k', label=labels[ind])
+            ax2.scatter(-0.5, 10, c=colour, s=50, zorder=1000, lw=2, edgecolors='k', label=labels[ind])
         else:
-            ax.scatter(-0.5, 10, c=colour, s=35, zorder=1000, lw=0, label=labels[ind])
-    ax.legend(loc=9, frameon=True)
-    plt.savefig(generations[0].run_hash + '_hullplot.pdf')
+            ax2.scatter(-0.5, 10, c=colour, s=35, zorder=1000, lw=0, label=labels[ind])
+    ax2.legend(loc=9, frameon=True)
+    sns.despine(ax=ax2, left=False, bottom=False)
+    ax2.set_xlim(-0.05, 1.05)
+    ax2.set_ylim(ax.get_ylim())
+    # ax2.set_xticks([])
+    ax2.set_yticks([])
+    if save:
+        plt.savefig(generations[0].run_hash + '_hullplot.pdf')
