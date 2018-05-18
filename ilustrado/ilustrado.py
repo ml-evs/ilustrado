ffecfd068a3ac6a81786ba247cfbd85aa7abf287
Matthew Evans <me388@cam.ac.uk>
2018-05-18 15:15:24 +0100

    MAINT: fixed references to matador.slurm and improved file handling

--- ilustrado/ilustrado.py
+++ ilustrado/ilustrado.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import subprocess as sp
 import logging
 import glob
+import shutil
 from os import listdir, makedirs, remove
 from os.path import isfile
 from time import sleep
@@ -365,9 +366,9 @@ class ArtificialSelector:
             logging.info('Found enough {} structures of target {}'.format(len(self.next_gen), self.population))
             if len(self.next_gen) < self.population:
                 logging.info('Initialising new relaxation jobs...')
-                import matador.slurm
+                import matador.compute.slurm
                 from matador.compute import reset_job_folder_and_count_remaining
-                slurm_dict = matador.slurm.get_slurm_env(fail_loudly=True)
+                slurm_dict = matador.compute.slurm.get_slurm_env(fail_loudly=True)
                 num_remaining = reset_job_folder_and_count_remaining()
 
                 # check if we can even finish this generation
@@ -401,8 +402,8 @@ class ArtificialSelector:
         # otherwise, generate a new unrelaxed generation and submit
         else:
             logging.info('Initialising new generation...')
-            import matador.slurm
-            slurm_dict = matador.slurm.get_slurm_env(fail_loudly=True)
+            import matador.compute.slurm
+            slurm_dict = matador.compute.slurm.get_slurm_env(fail_loudly=True)
             self.write_unrelaxed_generation()
             self.slurm_submit_relaxations_and_monitor(slurm_dict)
             logging.info('Exiting monitor...')
@@ -417,18 +418,18 @@ class ArtificialSelector:
 
         """
         # prepare script to relax this generation
-        import matador.slurm
+        import matador.compute.slurm
         logging.info('Preparing to submit slurm scripts...')
         relax_fname = '{}_relax.job'.format(self.run_hash)
         # override jobname with this run's hash to allow for selective job killing
         slurm_dict['SLURM_JOB_NAME'] = self.run_hash
         compute_string = 'run3 {}'.format(self.seed)
-        matador.slurm.write_slurm_submission_script(relax_fname,
-                                                    slurm_dict,
-                                                    compute_string,
-                                                    self.walltime_hrs,
-                                                    template=self.slurm_template,
-                                                    num_nodes=1)
+        matador.compute.slurm.write_slurm_submission_script(relax_fname,
+                                                            slurm_dict,
+                                                            compute_string,
+                                                            self.walltime_hrs,
+                                                            template=self.slurm_template,
+                                                            num_nodes=1)
         if self.max_num_nodes > self.max_attempts:
             self.max_num_nodes = self.max_attempts
             logging.info('Adjusted max num nodes to {}'.format(self.max_num_nodes))
@@ -436,21 +437,21 @@ class ArtificialSelector:
         # prepare script to read in results
         monitor_fname = '{}_monitor.job'.format(self.run_hash)
         compute_string = 'python {} >> ilustrado.out 2>> ilustrado.err'.format(self.entrypoint)
-        matador.slurm.write_slurm_submission_script(monitor_fname,
-                                                    slurm_dict,
-                                                    compute_string,
-                                                    1,
-                                                    template=self.slurm_template,
-                                                    num_nodes=1)
+        matador.compute.slurm.write_slurm_submission_script(monitor_fname,
+                                                            slurm_dict,
+                                                            compute_string,
+                                                            1,
+                                                            template=self.slurm_template,
+                                                            num_nodes=1)
         # submit jobs, if any exceptions, cancel all jobs
         try:
-            array_job_id = matador.slurm.submit_slurm_script(relax_fname, num_array_tasks=self.max_num_nodes)
+            array_job_id = matador.compute.slurm.submit_slurm_script(relax_fname, num_array_tasks=self.max_num_nodes)
             logging.info('Submitted job array: {}'.format(array_job_id))
-            monitor_job_id = matador.slurm.submit_slurm_script(monitor_fname, depend_on_job=array_job_id)
+            monitor_job_id = matador.compute.slurm.submit_slurm_script(monitor_fname, depend_on_job=array_job_id)
             logging.info('Submitted monitor job: {}'.format(monitor_job_id))
         except:
             logging.error('Something went wrong, trying to cancel all jobs.')
-            output = matador.slurm.scancel_all_matching_jobs(name=self.run_hash)
+            output = matador.compute.slurm.scancel_all_matching_jobs(name=self.run_hash)
             logging.error('scancel output: {}'.format(output))
             exit('Something went wrong, please check the log file.')
 
@@ -935,8 +936,11 @@ class ArtificialSelector:
         for doc in cursor:
             source = [src.replace('.castep', '.res') for src in doc['source'] if '-GA-' in src or src.endswith('.castep') or src.endswith('.res')]
             source = source[0].split('/')[-1]
-            if len(source) == 0:
+            if not source:
                 print('Issue writing {}'.format(doc['source']))
                 continue
             else:
                 doc2res(doc, '{}/{}'.format(path, source), overwrite=False, hash_dupe=False)
+            if isfile('completed/{}'.format(source.replace('.res', '.castep'))):
+                shutil.copy('completed/{}'.format(source.replace('.res', '.castep')),
+                            '{}/{}'.format(path, source.replace('.res', '.castep')))
